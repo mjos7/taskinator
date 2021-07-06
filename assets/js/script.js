6191f24241be70c4e125cbb47b3a929db04d97d4
mjos7 <markjnjoseph@gmail.com>
2021-07-05 21:15:55 -0700

    delete task function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 var taskIdCounter = 0;
+
 var formEl = document.querySelector('#task-form');
 var tasksToDoEl = document.querySelector('#tasks-to-do');
+var pageContentEl = document.querySelector('#page-content');
 
 var taskFormHandler = function (event) {
   event.preventDefault();
@@ -91,5 +93,22 @@ var createTaskActions = function (taskId) {
 
   return actionContainerEl;
 };
+
+var taskButtonHandler = function (event) {
+  if (event.target.matches('.delete-btn')) {
+    // get the element's task id
+    var taskId = event.target.getAttribute('data-task-id');
+    deleteTask(taskId);
+  }
+};
+
+var deleteTask = function (taskId) {
+  var taskSelected = document.querySelector(
+    ".task-item[data-task-id='" + taskId + "']"
+  );
+  taskSelected.remove();
+};
+
+pageContentEl.addEventListener('click', taskButtonHandler);
 
 formEl.addEventListener('submit', taskFormHandler);
