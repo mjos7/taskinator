b86cdbc2d9ae024ed4ba8b0a58cfc7b8a98a8249
mjos7 <markjnjoseph@gmail.com>
2021-07-05 19:46:37 -0700

    add event listener to form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 // Target button object
-var buttonEl = document.getElementById('save-task');
+var formEl = document.querySelector('#task-form');
 var tasksToDoEl = document.querySelector('#tasks-to-do');
 
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+  event.preventDefault();
+
   var listItemEl = document.createElement('li');
   listItemEl.className = 'task-item';
   listItemEl.textContent = 'This is a new task.';
   tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener('click', createTaskHandler);
+formEl.addEventListener('submit', createTaskHandler);
